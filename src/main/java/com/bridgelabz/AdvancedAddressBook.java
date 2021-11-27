61f96b58131ca641ba5c609a957be209d930f666
saranyellanki <saran.yellanki12@gmail.com>
2021-11-27 07:30:50 +0530

    Added: Welcome message to the project

---
+++ src/main/java/com/bridgelabz/AdvancedAddressBook.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AdvancedAddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Advanced Address Book");
+    }
+}
